dca0e579ca82ce6a475f01fae724219728ef2f4b
Sumit Dey <sumitdeyonline@gmail.com>
2019-02-19 21:06:47 -0800

    Apply Now

--- src/app/pages/jobdetails/applyjob/applyjob.component.ts
+++ src/app/pages/jobdetails/applyjob/applyjob.component.ts
@@ -10,6 +10,8 @@ import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular
 export class ApplyjobComponent implements OnInit {
 
   applyJobForm: FormGroup;
+  selectedFiles: FileList;
+  filleUploadEnabled: boolean = false;
 
   constructor(private dialogRef: MatDialogRef<ApplyjobComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any, fb: FormBuilder)
@@ -20,6 +22,8 @@ export class ApplyjobComponent implements OnInit {
         'LastName': '',
         'Email': '',
         'PhoneNumber': '',
+        'CoverLetter':'',
+        'fileUpload':'',
       });
 
     }
@@ -28,6 +32,15 @@ export class ApplyjobComponent implements OnInit {
   }
 
 
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+    if (this.validateFile(this.selectedFiles.item(0).name)) {
+      this.filleUploadEnabled = true;
+    } else {
+      this.filleUploadEnabled = false;
+    }
+  } 
+
   close() {
     this.dialogRef.close();
   }
@@ -35,6 +48,22 @@ export class ApplyjobComponent implements OnInit {
   applyNow(form:NgForm){
     console.log(form);
     this.close();
+  }
+
+  upload() {
+    const file = this.selectedFiles.item(0);
+    console.log("this.selectedFiles.item(0) :::::: => "+this.selectedFiles.item(0).name);
+  }
+
+  validateFile(fileName: string) {
+    let ext = fileName.substring(fileName.lastIndexOf('.')+1);
+    console.log("EXTESTION :::::::$$$&&&&&&& "+ext);
+    if ((ext.toLowerCase() == 'doc') || (ext.toLowerCase() == 'docx') || (ext.toLowerCase() == 'pdf') || (ext.toLowerCase() == 'ppt') || (ext.toLowerCase() == 'pptx')) {
+      return true;
+    } else {
+      return false;
+    }
+
   }
 
 }
