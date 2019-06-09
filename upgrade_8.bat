a930c847128be414f53310ff47778e3a8db7ee3a
Sumit Dey <sumitdeyonline@gmail.com>
2019-06-08 22:07:01 -0700

    Angular Update 8

---
+++ upgrade_8.bat
@@ -0,0 +1,5 @@
+ng update @angular/cli --next --force
+ng update @angular/core --next --force
+ng update @angular/core --next --force
+ng update @angular/material --next --force
+ng update rxjs --force
